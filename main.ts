acad875283130a3a9bb3da6b88f3bdabe1f5429d
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-22 10:46:57 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
-	
+function Stuff () {
+    basic.showIcon(IconNames.Duck)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
+}
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        Stuff()
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    Stuff()
+})
+basic.showIcon(IconNames.Angry)
